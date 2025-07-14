9037ad3e72fe8310a7e5aba93e9b4bc251826222
Hasan Coder 66 <hasanashraf874@gmail.com>
2025-07-14 14:21:33 +0500

    get all interviews api done

--- ai-recruiter-BE/Routes/interview.js
+++ ai-recruiter-BE/Routes/interview.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { CreateInterviewAndGenerateQuestions } from "../Controllers/interview.js";
+import { CreateInterviewAndGenerateQuestions, getAllInterviews } from "../Controllers/interview.js";
 
 const interviewRoutes = express.Router();
 
 
 // Example route for creating an interview
 interviewRoutes.post("/create", CreateInterviewAndGenerateQuestions);
+interviewRoutes.get("/all-interviews", getAllInterviews);
 
 export default interviewRoutes;
